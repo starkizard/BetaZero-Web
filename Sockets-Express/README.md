661a2812e95d1dbbe23b46a9b91499b3635317b1
Kushagra Manish <kushagra.manish01@gmail.com>
2020-10-24 21:54:24 +0530

    "Added Node, Express, Sockets modules for the main chess arena for testin

---
+++ Sockets-Express/README.md
@@ -0,0 +1,8 @@
+# Real Chess
+<ol>
+<li>Open terminal</li>
+<li>Cd to this directory</li>
+<li>Run this command - "node app.js"</li>
+<li>Open "localhost:3000" on browser</li>
+<li>Enjoy!
+</ol>
---
+++ Sockets-Express/public/default.js
@@ -0,0 +1,298 @@
+
+(function () {
+
+    
+    
+    WinJS.UI.processAll().then(function () {
+      var $status = $('#status')
+      var $fen = $('#fen')
+      var $pgn = $('#pgn')
+
+      var whiteSquareGrey = '#a9a9a9'
+      var blackSquareGrey = '#696969'
+      var socket, serverGame;
+      var username, playerColor;
+      var game, board;
+      var usersOnline = [];
+      var myGames = [];
+      
+      socket = io();
+
+           
+      //////////////////////////////
+      // Socket.io handlers
+      ////////////////////////////// 
+      
+      socket.on('login', function(msg) {
+            usersOnline = msg.users;
+            updateUserList();
+            
+            myGames = msg.games;
+            updateGamesList();
+      });
+      
+      socket.on('joinlobby', function (msg) {
+        addUser(msg);
+      });
+      
+       socket.on('leavelobby', function (msg) {
+        removeUser(msg);
+      });
+      
+      socket.on('gameadd', function(msg) {
+      });
+      
+      socket.on('resign', function(msg) {
+            if (msg.gameId == serverGame.id) {
+
+              socket.emit('login', username);
+
+              $('#page-lobby').show();
+              $('#page-game').hide();
+            }            
+      });
+                  
+      socket.on('joingame', function(msg) {
+
+        console.log("joined as game id: " + msg.game.id );   
+        playerColor = msg.color;
+        initGame(msg.game);
+        updateStatus();
+        $('#page-lobby').hide();
+        $('#page-game').show();
+        
+      });
+        
+      socket.on('move', function (msg) {
+        updateStatus();
+        if (serverGame && msg.gameId === serverGame.id) {
+          updateStatus();
+           game.move(msg.move);
+           board.position(game.fen());
+        }
+      });
+     
+      
+      socket.on('logout', function (msg) {
+        removeUser(msg.username);
+      });
+      
+
+      
+      //////////////////////////////
+      // Menus
+      ////////////////////////////// 
+      $('#login').on('click', function() {
+        username = $('#username').val();
+        
+        if (username.length > 0) {
+            $('#userLabel').text(username);
+            socket.emit('login', username);
+            
+            $('#page-login').hide();
+            $('#page-lobby').show();
+        } 
+      });
+      
+      $('#game-back').on('click', function() {
+        socket.emit('login', username);
+        
+        $('#page-game').hide();
+        $('#page-lobby').show();
+      });
+      
+      $('#game-resign').on('click', function() {
+        socket.emit('resign', {userId: username, gameId: serverGame.id});
+        
+        socket.emit('login', username);
+        $('#page-game').hide();
+        $('#page-lobby').show();
+      });
+      
+      var addUser = function(userId) {
+        usersOnline.push(userId);
+        updateUserList();
+      };
+    
+     var removeUser = function(userId) {
+          for (var i=0; i<usersOnline.length; i++) {
+            if (usersOnline[i] === userId) {
+                usersOnline.splice(i, 1);
+            }
+         }
+         
+         updateUserList();
+      };
+      
+      var updateGamesList = function() {
+        document.getElementById('gamesList').innerHTML = '';
+        myGames.forEach(function(game) {
+          $('#gamesList').append($('<button>')
+                        .text('#'+ game)
+                        .on('click', function() {
+                          socket.emit('resumegame',  game);
+                        }));
+        });
+      };
+      
+      var updateUserList = function() {
+        document.getElementById('userList').innerHTML = '';
+        usersOnline.forEach(function(user) {
+          $('#userList').append($('<button>')
+                        .text(user)
+                        .on('click', function() {
+                          socket.emit('invite',  user);
+                        }));
+        });
+      };
+           
+      //////////////////////////////
+      // Chess Game
+      ////////////////////////////// 
+      
+      var initGame = function (serverGameState) {
+
+        serverGame = serverGameState; 
+        
+          var cfg = {
+            draggable: true,
+            showNotation: false,
+            orientation: playerColor,
+            position: serverGame.board ? serverGame.board : 'start',
+            onDragStart: onDragStart,
+            onDrop: onDrop,
+            onMouseoutSquare: onMouseoutSquare,
+            onMouseoverSquare: onMouseoverSquare,
+            onSnapEnd: onSnapEnd
+          };
+               
+          
+          game = serverGame.board ? new Chess(serverGame.board) : new Chess();
+          board = new ChessBoard('game-board', cfg);
+
+         
+      }
+      
+      var removeGreySquares = function() {
+          $('#Board .square-55d63').css('background', '')
+        }
+
+      var greySquare =function(square) {
+          var $square = $('#Board .square-' + square)
+
+          var background = whiteSquareGrey
+          if ($square.hasClass('black-3c85d')) {
+            background = blackSquareGrey
+          }
+
+          $square.css('background', background)
+        }
+
+       
+      // do not pick up pieces if the game is over
+      // only pick up pieces for the side to move
+      var onDragStart = function(source, piece, position, orientation) {
+        if (game.game_over() === true ||
+            (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+            (game.turn() === 'b' && piece.search(/^w/) !== -1) ||
+            (game.turn() !== playerColor[0])) {
+          return false;
+        }
+
+      };  
+      
+    
+      
+      var onDrop = function(source, target) {
+        // see if the move is legal
+        var move = game.move({
+          from: source,
+          to: target,
+          promotion: 'q' // NOTE: always promote to a queen for example simplicity
+        });
+      
+        // illegal move
+        updateStatus();
+        if (move === null) { 
+          return 'snapback';
+        } else {
+           
+           socket.emit('move', {move: move, gameId: serverGame.id, board: game.fen()});
+           
+        }
+      
+      };
+     var onMouseoverSquare = function(square, piece) {
+      updateStatus();
+    // get list of possible moves for this square
+          var moves = game.moves({
+            square: square,
+            verbose: true
+          })
+
+          // exit if there are no moves available for this square
+          if (moves.length === 0) return
+
+          // highlight the square they moused over
+          greySquare(square)
+
+          // highlight the possible squares for this piece
+          for (var i = 0; i < moves.length; i++) {
+            greySquare(moves[i].to)
+          };
+        };
+
+  var updateStatus = function() {
+  var status = ''
+  
+  var moveColor = 'White'
+  if (game.turn() === 'b') {
+    moveColor = 'Black'
+  }
+  
+  // checkmate?
+  if (game.in_checkmate()) {
+    status = 'Game over, ' + moveColor + ' is in checkmate.'
+  }
+  
+  // draw?
+  else if (game.in_draw()) {
+    status = 'Game over, drawn position'
+  }
+  
+  // game still on
+  else {
+    status = moveColor + ' to move'
+    
+    // check?
+    if (game.in_check()) {
+      status += ', ' + moveColor + ' is in check'
+    }
+  }
+  
+  $status.html(status)
+  $fen.html(game.fen())
+  $pgn.html(game.pgn())
+  
+};
+
+      var onMouseoutSquare = function(square, piece) {
+            removeGreySquares()
+          };
+
+
+
+
+      
+      // update the board position after the piece snap 
+      // for castling, en passant, pawn promotion
+      var onSnapEnd = function() {
+        board.position(game.fen());
+        updateStatus();
+
+      };
+    });
+})();
+
+
+
